35eb4931218544877aa273495f66f77962e9dd38
SigneKirkBrødbæk#5330 <signebroedbaek@gmail.com>
2022-05-21 12:46:00 +0200

    creaste base_model_inspection + clean up tl

---
+++ base_model_inspection.py
@@ -0,0 +1,42 @@
+from tensorflow.keras.applications import InceptionV3, EfficientNetV2M
+from contextlib import redirect_stdout
+from keras.utils.vis_utils import plot_model
+
+# shape of input images
+input_shape = (299,299,3)
+
+# define base models for inspection
+inceptionv3_base = InceptionV3( 
+    input_shape=input_shape, 
+    weights="imagenet", # include pre-trained weights
+    include_top=False) # don't include top/last fully connected layer
+
+efficientnetv2m_base = EfficientNetV2M(
+                input_shape=input_shape,
+                weights='imagenet', # include pre-trained weights from training on imagenet
+                include_top=False) # don't include top/last fully connected layer
+
+#  visualize layer names and layer indices to figure out (together with the plot_model png) how many layers we should freeze
+# in order to freeze the top two blocks 
+for i, layer in enumerate(inceptionv3_base.layers):
+    print(i, layer.name)
+
+for i, layer in enumerate(efficientnetv2m_base.layers):
+    print(i, layer.name)
+
+# save summaries
+with open(f'output/inceptionv3/inceptionv3_base_summary.txt', 'w') as f:
+    with redirect_stdout(f):
+        inceptionv3_base.summary()
+
+with open(f'output/efficientnetv2m/efficientnetv2m_base_summary.txt', 'w') as f:
+    with redirect_stdout(f):
+        efficientnetv2m_base.summary()
+
+print("number of layers of inceptionv3_base:", len(inceptionv3_base.layers))
+print("number of layers of inceptionv3_base:", len(efficientnetv2m_base.layers))
+
+#plot model architecture
+plot_model(inceptionv3_base, f'output/inceptionv3/inceptionv3_base_architecture.png', show_shapes=True)
+plot_model(efficientnetv2m_base, f'output/efficientnetv2m/efficientnetv2m_base_architecture.png', show_shapes=True)
+
--- transfer_learning.py
+++ transfer_learning.py
@@ -8,8 +8,7 @@ from keras.utils.vis_utils import plot_model
 from contextlib import redirect_stdout
 import pandas as pd
 
-from tensorflow.keras.applications import InceptionV3
-from tensorflow.keras.layers import Dense, Dropout, Flatten, BatchNormalization, Activation # MaxPooling2D, AveragePooling2D
+
 
 #import functions
 from src.utils import *
@@ -70,39 +69,6 @@ print('Number of images in x_test', x_test.shape[0], ', x_test shape is', x_test
 print('Number of images in x_val', x_val.shape[0], ', x_val shape is', x_val.shape)
 print('Total number of images:', x_train.shape[0] + x_test.shape[0] + x_val.shape[0])
 
-################################################ redundant ###################################################
-#create model
-inceptionv3 = transfer_learning_model('inceptionv3', input_shape) 
-efficientnetv2 = transfer_learning_model('efficientnetv2m', input_shape) 
-
-# Print layers for inspection
-inceptionv3.summary()
-print(len(inceptionv3.layers))
-efficientnetv2.summary()
-print(len(efficientnetv2.layers))
-
-# define base models for inspection
-inceptionv3_base = InceptionV3( 
-    input_shape=input_shape, # define input/image shape
-    weights="imagenet", # include pre-trained weights
-    include_top=False) # don't include top/last fully connected layer
-
-efficientnetv2_base = EfficientNetV2M(
-                input_shape=input_shape, # define input/image shape
-                weights='imagenet', # include pre-trained weights from training on imagenet
-                include_top=False) # don't include top/last fully connected layer
-
-inceptionv3_base.summary()
-print("number of layers of inceptionv3_base:", len(inceptionv3_base.layers))
-efficientnetv2_base.summary()
-print("number of layers of inceptionv3_base:", len(efficientnetv2_base.layers))
-
-
-plot_model(inceptionv3_base, f'output/{inceptionv3_base}_architecture.png', show_shapes=True)
-
-plot_model(efficientnetv2_base, f'output/{efficientnetv2_base}_architecture.png', show_shapes=True)
-
-################################################ redundant ###################################################
 
 #prepare names of base models to loop through
 base_models = ['inceptionv3'] #, 'efficientnetv2m']
@@ -127,27 +93,21 @@ for base_model in base_models:
 
   # Fit initial model (train on a few epochs before unfreezing top layers of base model for fine-tuning)
   history = model.fit(x=x_train,y=y_train, epochs=5, validation_data=(x_val, y_val))
-  # started 18:12 - ended: 
 
-  ############## IMPLEMENT THIS ##############
-  # https://keras.io/guides/transfer_learning/
-  # https://keras.io/api/applications/#usage-examples-for-image-classification-models
-  # https://medium.com/analytics-vidhya/transfer-learning-using-inception-v3-for-image-classification-86700411251b
-
-  # unfreeze top 2 conv blocks so they can be fine-tuned during remaining training
+  # unfreeze top 2 blocks so they can be fine-tuned during the remaining training
   if base_model == 'inceptionv3':
     for layer in model.layers[:249]:
       layer.trainable = False
     for layer in model.layers[249:]:
       layer.trainable = True
   elif base_model == 'efficientnetv2m':
-    for layer in model.layers[:249]:
+    for layer in model.layers[:645]:
       layer.trainable = False
-    for layer in model.layers[249:]:
+    for layer in model.layers[645:]:
       layer.trainable = True
 
   # recompile the model for modifications to take effect
-  model.compile(keras.optimizers.Adam(1e-5), # very low learning rate
+  model.compile(keras.optimizers.Adam(0.001), # low learning rate
         loss='sparse_categorical_crossentropy', 
         metrics = ['accuracy']) 
 
@@ -163,10 +123,10 @@ for base_model in base_models:
 
   #save classification report
   clsf_report = pd.DataFrame(classification_report(y_test, y_pred_bool, output_dict=True)).transpose()
-  clsf_report.to_csv(f'output/{base_model}_clsf_report.csv', index= True)
+  clsf_report.to_csv(f'output/{base_model}/{base_model}_clsf_report.csv', index= True)
 
   #plot model architecture
-  plot_model(model, f'output/{base_model}_architecture.png', show_shapes=True)
+  plot_model(model, f'output/{base_model}/{base_model}_architecture.png', show_shapes=True)
 
   # confusion matrix
   keras.metrics.confusion_matrix(y_test, y_pred)
@@ -179,7 +139,7 @@ for base_model in base_models:
   plt.ylabel('Accuracy value (%)')
   plt.xlabel('No. epoch')
   plt.legend(loc="upper right")
-  plt.savefig(f'output/{base_model}_loss.jpg')
+  plt.savefig(f'output/{base_model}/{base_model}_loss.jpg')
   plt.clf()
 
   # Plot history: Accuracy
@@ -189,7 +149,10 @@ for base_model in base_models:
   plt.ylabel('Accuracy value (%)')
   plt.xlabel('No. epoch')
   plt.legend(loc="upper left")
-  plt.savefig(f'output/{base_model}_accuracy.jpg')
+  plt.savefig(f'output/{base_model}/{base_model}_accuracy.jpg')
   plt.clf()
 
-
+#transfer learning and fine-tuning sources
+  # https://keras.io/guides/transfer_learning/
+  # https://keras.io/api/applications/#usage-examples-for-image-classification-models
+  # https://medium.com/analytics-vidhya/transfer-learning-using-inception-v3-for-image-classification-86700411251b
