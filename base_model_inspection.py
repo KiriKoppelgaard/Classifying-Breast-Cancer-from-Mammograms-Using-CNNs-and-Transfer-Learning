60fcfa74c9ae6a6cd2758c8f1f01542107749fd2
SigneKirkBrødbæk#5330 <signebroedbaek@gmail.com>
2022-05-26 10:59:41 +0200

    add efficientnetv2s

--- base_model_inspection.py
+++ base_model_inspection.py
@@ -5,12 +5,12 @@ It is used to find layer numbers of the layer we need to freeze from, so we can 
 Was run interactively. 
 '''
 
-from tensorflow.keras.applications import InceptionV3, EfficientNetV2M
+from tensorflow.keras.applications import InceptionV3, EfficientNetV2S
 from contextlib import redirect_stdout
 from keras.utils.vis_utils import plot_model
 
 # shape of input images
-input_shape = (299,299,3)
+input_shape = (100,100,3)
 
 # define base models for inspection
 inceptionv3_base = InceptionV3( 
@@ -18,21 +18,21 @@ inceptionv3_base = InceptionV3(
     weights="imagenet", # include pre-trained weights
     include_top=False) # don't include top/last fully connected layer
 
-efficientnetv2m_base = EfficientNetV2M(
+efficientnetv2_base = EfficientNetV2S(
                 input_shape=input_shape,
                 weights='imagenet', # include pre-trained weights from training on imagenet
                 include_top=False) # don't include top/last fully connected layer
 
 # plot model architecture
 plot_model(inceptionv3_base, f'output/inceptionv3/inceptionv3_base_architecture.png', show_shapes=True)
-plot_model(efficientnetv2m_base, f'output/efficientnetv2m/efficientnetv2m_base_architecture.png', show_shapes=True)
+plot_model(efficientnetv2_base, f'output/efficientnetv2s/efficientnetv2s_base_architecture.png', show_shapes=True)
 
 # visualize layer names and layer indices 
 # used together with the plot_model png to figure out how many layers we should freeze in order to unfreeze the top two blocks 
 for i, layer in enumerate(inceptionv3_base.layers):
     print(i, layer.name)
 
-for i, layer in enumerate(efficientnetv2m_base.layers):
+for i, layer in enumerate(efficientnetv2_base.layers):
     print(i, layer.name)
 
 # save summaries
@@ -40,11 +40,11 @@ with open(f'output/inceptionv3/inceptionv3_base_summary.txt', 'w') as f:
     with redirect_stdout(f):
         inceptionv3_base.summary()
 
-with open(f'output/efficientnetv2m/efficientnetv2m_base_summary.txt', 'w') as f:
+with open(f'output/efficientnetv2s/efficientnetv2S_base_summary.txt', 'w') as f:
     with redirect_stdout(f):
-        efficientnetv2m_base.summary()
+        efficientnetv2_base.summary()
 
 # print number of layers
 print("number of layers of inceptionv3_base:", len(inceptionv3_base.layers))
-print("number of layers of inceptionv3_base:", len(efficientnetv2m_base.layers))
+print("number of layers of inceptionv3_base:", len(efficientnetv2_base.layers))
 
